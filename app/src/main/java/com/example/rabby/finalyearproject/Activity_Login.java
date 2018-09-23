d3302ddf5b9d8065113ba0196b92262c8aa4040a
Md. Golam Rabby Jim <jim15-5551@diu.bd>
2018-09-24 04:33:28 +0600

    Initial commit

---
+++ app/src/main/java/com/example/rabby/finalyearproject/Activity_Login.java
@@ -0,0 +1,13 @@
+package com.example.rabby.finalyearproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__login);
+    }
+}
